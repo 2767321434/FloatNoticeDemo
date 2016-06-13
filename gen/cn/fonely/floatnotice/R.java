1fecbe2e03042b11b41f972da4a86b5fcc11af87
2767321434 <2767321434@qq.com>
2016-06-13 09:46:12 +0800

    添加帮助

--- gen/cn/fonely/floatnotice/R.java
+++ gen/cn/fonely/floatnotice/R.java
@@ -22,43 +22,59 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_back=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_remove=0x7f020002;
-        public static final int msg=0x7f020003;
-        public static final int number_bg=0x7f020004;
+        public static final int help_1=0x7f020000;
+        public static final int help_2=0x7f020001;
+        public static final int help_3=0x7f020002;
+        public static final int help_4=0x7f020003;
+        public static final int help_5=0x7f020004;
+        public static final int help_6=0x7f020005;
+        public static final int ic_back=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int ic_remove=0x7f020008;
+        public static final int msg=0x7f020009;
+        public static final int number_bg=0x7f02000a;
     }
     public static final class id {
-        public static final int app_check=0x7f080007;
-        public static final int app_icon_view=0x7f080005;
-        public static final int app_name_view=0x7f080006;
+        public static final int app_check=0x7f08000e;
+        public static final int app_icon_view=0x7f08000c;
+        public static final int app_name_view=0x7f08000d;
         public static final int applist_layout=0x7f080000;
         public static final int applist_view=0x7f080001;
-        public static final int back_btn=0x7f080011;
+        public static final int back_btn=0x7f080018;
         public static final int cancel_all=0x7f080003;
-        public static final int del_btn=0x7f080010;
-        public static final int fragment1=0x7f080004;
-        public static final int large_icon=0x7f080009;
-        public static final int nIcon=0x7f08000d;
-        public static final int nText=0x7f08000f;
-        public static final int nTitle=0x7f08000e;
-        public static final int number=0x7f08000b;
-        public static final int pack_layout=0x7f080008;
+        public static final int del_btn=0x7f080017;
+        public static final int fragment1=0x7f08000b;
+        public static final int imageView1=0x7f080005;
+        public static final int imageView2=0x7f080006;
+        public static final int imageView3=0x7f080007;
+        public static final int imageView4=0x7f080008;
+        public static final int imageView5=0x7f080009;
+        public static final int imageView6=0x7f08000a;
+        public static final int large_icon=0x7f080010;
+        public static final int nIcon=0x7f080014;
+        public static final int nText=0x7f080016;
+        public static final int nTitle=0x7f080015;
+        public static final int number=0x7f080012;
+        public static final int pack_layout=0x7f08000f;
+        public static final int scrollView1=0x7f080004;
         public static final int select_all=0x7f080002;
-        public static final int small_icon=0x7f08000a;
-        public static final int unpack_layout=0x7f08000c;
+        public static final int small_icon=0x7f080011;
+        public static final int unpack_layout=0x7f080013;
     }
     public static final class layout {
         public static final int activity_app_list=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int app_item=0x7f030002;
-        public static final int pack_layout=0x7f030003;
-        public static final int unpack_layout=0x7f030004;
+        public static final int activity_help=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int app_item=0x7f030003;
+        public static final int pack_layout=0x7f030004;
+        public static final int unpack_layout=0x7f030005;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060004;
         public static final int title_activity_app_list=0x7f060002;
+        public static final int title_activity_help=0x7f060003;
     }
     public static final class style {
         /** 
---
+++ src/cn/fonely/floatnotice/HelpActivity.java
@@ -0,0 +1,15 @@
+package cn.fonely.floatnotice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_help);
+    }
+}
